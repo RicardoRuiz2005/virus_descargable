23348e20d10f23d50f53407cbdcd3209c0d674b0
RicardoRuiz2005 <150260615+RicardoRuiz2005@users.noreply.github.com>
2024-12-12 14:58:41 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Redirige a Rickroll si se elige el agua de horchata
+document.getElementById("horchata-button").addEventListener("click", function() {
+    window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=uBZ2IbHhh573r7Ss";
+});
+
+// Redirige a la invitación si se elige el agua de jamaica
+document.getElementById("jamaica-button").addEventListener("click", function() {
+    window.location.href = "invitacion.html"; // Redirige a la página de invitación
+});
